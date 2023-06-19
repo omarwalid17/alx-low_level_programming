22aa9f67a5ea3983eedb39a8b50bb07d2ec386bb
root <root@DESKTOP-DBDGUMP.localdomain>
2023-06-19 15:43:50 +0300

    hi

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,20 @@
+#include <unistd.h>
+#include <stdio.h>
+
+
+/**
+ * main - Entry point
+ *
+ * Description: A c programm prints quote with write function
+ *              write(int fd.const void *buf.size_t count)
+ *
+ * Return: 1 (Not Succecced)
+*/
+
+int main(void)
+{
+	char quo[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
+	write(1, quo, sizeof(quo));
+	return (1);
+}
