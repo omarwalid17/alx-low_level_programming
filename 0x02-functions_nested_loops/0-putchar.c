764a7d24639f67ef4da3339c760a1d27736f757f
root <root@DESKTOP-DBDGUMP.localdomain>
2023-06-22 11:17:13 +0300

    mm

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -2,9 +2,9 @@
 
 /**
  * main - Entry Point
- * 
+ *
  * Description: Prints _putchar with putchar protoype
- * 
+ *
  * Return: Always 0 (Success)
 */
 
@@ -13,7 +13,7 @@ int main(void)
 	char str[] = "_putchar";
 	int ch;
 
-	for(ch = 0; ch < 8; ch++)
+	for (ch = 0; ch < 8; ch++)
 		_putchar(str[ch]);
 	_putchar('\n');
 
