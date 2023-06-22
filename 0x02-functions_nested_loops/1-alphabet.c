180c9e7da18c24c4bf1e66d8c97360a6b7a997d7
root <root@DESKTOP-DBDGUMP.localdomain>
2023-06-22 11:42:20 +0300

    8

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,8 +1,9 @@
 #include "main.h"
 
 /**
- * print_alphabet: print the charaters from a to z
+ * print_alphabet - print the charaters from a to z in lowercase
  *
+ * Return: Always 0 (Success)
 */
 
 void print_alphabet(void)
