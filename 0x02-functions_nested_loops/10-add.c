3f02add2c9f47149720aaed47ec51bbc7573cfa9
root <root@DESKTOP-DBDGUMP.localdomain>
2023-06-24 10:06:27 +0300

    Omar Walied

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+*add - a function to check if c is lowercase
+*
+* @a : First Input
+* @b : Second Input
+*Return: Always 0 (Success)
+*/
+
+int add(int a, int b)
+{
+	int sum = a + b;
+
+	return (sum);
+}
