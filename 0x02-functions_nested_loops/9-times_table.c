44e59508fcf4059966a914e8b17b4c14d4173f3e
root <root@DESKTOP-DBDGUMP.localdomain>
2023-06-24 09:58:00 +0300

    Omar Walied

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+* times_table - a function to check if c is lowercase
+*
+* Return: Always 0 (Success)
+*/
+
+void times_table(void)
+{
+	int num, multi, prod;
+
+	for (num = 0 ; num <= 9 ; num++)
+	{
+		_putchar(48);
+		for (multi = 1 ; multi <= 9 ; multi++)
+		{
+		_putchar(',');
+		_putchar(' ');
+
+			prod = num * multi;
+
+			if (prod <= 9)
+				_putchar(' ')
+			else
+				_putchar((prod / 10) + 48);
+
+		_putchar((prod % 10) + 48);
+		}
+	}
+	_putchar('\n');
+}
