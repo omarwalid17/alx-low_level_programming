67a0c3b4b1ad997d425f3ebb4afb90ffa211e314
root <root@DESKTOP-DBDGUMP.localdomain>
2023-06-24 10:48:41 +0300

    Omar Walied

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+*print_to_98 - a function to check if c is lowercase
+*
+* @n : First Input
+*
+*Return: Always 0 (Success)
+*/
+
+void print_to_98(int n)
+{
+	int count;
+
+	if (n > 98)
+		for (count = n ; n > 98 ; count--)
+			printf("%d, ", count);
+	else
+		for (count = n ; n < 98 ; count++)
+			printf("%d, ", count);
+	printf("98\n");
+}
