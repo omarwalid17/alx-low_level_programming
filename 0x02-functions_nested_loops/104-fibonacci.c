9ac68b9f407a514468d165869084b3d2d0f5307e
root <root@DESKTOP-DBDGUMP.localdomain>
2023-06-25 09:39:11 +0300

    Omar Walied

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -7,42 +7,43 @@
 
 int main(void)
 {
-	int i;
-	unsigned long int a1, a2, b1, b2, c1, c2;
+    int i;
+    unsigned long int a1, a2, b1, b2, c1, c2;
 
-	a1 = 0;
-	a2 = 1;
-	b1 = 0;
-	b2 = 2;
+    a1 = 0;
+    a2 = 1;
+    b1 = 0;
+    b2 = 2;
 
-	printf("%lu, %lu", a2, b2);
+    printf("%lu, %lu", a2, b2);
 
-	for (i = 0; i < 96; i++)
-	{
-		c1 = a1 + b1;
-		c2 = a2 + b2;
+    for (i = 0; i < 96; i++)
+    {
+        c1 = a1 + b1;
+        c2 = a2 + b2;
+        if (c2 > 999999999)
+        {
+            c1++;
+            c2 %= 1000000000;
+        }
+        printf(", ");
+        if (c1 == 0) // added conditional statement here
+        {
+            printf("%lu", c2); // print only the second part
+        }
+        else
+        {
+            printf("%lu%lu", c1, c2); // print both parts
+        }
+        a1 = b1;
+        a2 = b2;
+        b1 = c1;
+        b2 = c2;
+        if (i == 95) // added conditional statement here
+        {
+            printf("\n%lu\n", c2); // print only the second part of the last number
+        }
+    }
 
-		if (c2 > 999999999)
-		{
-			c1++;
-			c2 %= 1000000000;
-		}
-		printf(", ");
-		if (c1 == 0)
-		{
-			printf("%lu", c2);
-		}
-		else
-		{
-			printf("%lu%lu", c1, c2);
-		}
-		a1 = b1;
-		a2 = b2;
-		b1 = c1;
-		b2 = c2;
-	}
-
-	printf("\n");
-
-	return (0);
+    return (0);
 }
