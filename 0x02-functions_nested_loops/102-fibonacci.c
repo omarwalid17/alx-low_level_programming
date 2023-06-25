9f7eb66e95b764049887dfb16cf11d2c012f8901
root <root@DESKTOP-DBDGUMP.localdomain>
2023-06-25 07:57:44 +0300

    Omar Walied

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -9,34 +9,36 @@
 
 int main(void)
 {
-    int count = 3; /* this is so because the first 2 members have been given already. My loop will therefore begin from the 3rd one */
+	int count = 3;
+	/* My loop will therefore begin from the 3rd one */
 
-    long int first = 1, second = 2;
-    long int next = first + second;
+	long int first = 1, second = 2;
+	long int next = first + second;
 
-    printf("%lu, ", first);
-    printf("%lu, ", second);
+	printf("%lu, ", first);
+	printf("%lu, ", second);
 
-    while (count <= 50)
-    {
-        /* Let's check if we are at the end of the list, if we are close with a new line */
-        if (count == 50)
-        {
-            printf("%lu\n", next);
-         }
-         else  /* if we are not at the end of the list, add a comma after the number */
-         { 
-           printf("%lu, ", next); 
-         }
+	while (count <= 50)
+	{
+		/* if we are close with a new line */
+		if (count == 50)
+		{
+			printf("%lu\n", next);
+		}
+		else
+		/* if we are not at the end of the list, add a comma after the number */
+		{
+			printf("%lu, ", next);
+		}
 
-        /* Reset the variables to get the next number */
-        first = second;
-        second = next;
+		/* Reset the variables to get the next number */
+		first = second;
+		second = next;
 
-        /* after resetting the variables, you need to find the next number */
-        next = first + second; 
-        count++;
-    }
+		/* after resetting the variables, you need to find the next number */
+		next = first + second;
+		count++;
+	}
 
-    return (0);
+	return (0);
 }
