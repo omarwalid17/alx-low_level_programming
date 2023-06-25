65af2376dd5154b07ef18478eadd28656395e2a8
root <root@DESKTOP-DBDGUMP.localdomain>
2023-06-25 09:43:58 +0300

    Omar Walied

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,49 +1,52 @@
 #include <stdio.h>
 
-/**
- * main - prints the first 98 Fibonacci numbers
- * Return: 0
- */
 
-int main(void)
-{
-    int i;
-    unsigned long int a1, a2, b1, b2, c1, c2;
+	/**
+	 * main - fibonacci <3
+	 *
+	 * Purpose - no hardcode
+	 *
+	 * Return:  (Success)
+	 */
 
-    a1 = 0;
-    a2 = 1;
-    b1 = 0;
-    b2 = 2;
 
-    printf("%lu, %lu", a2, b2);
+	int main(void)
+	{
+		unsigned long int i;
+		unsigned long int bef = 1;
+		unsigned long int aft = 2;
+		unsigned long int l = 1000000000;
+		unsigned long int bef1;
+		unsigned long int bef2;
+		unsigned long int aft1;
+		unsigned long int aft2;
 
-    for (i = 0; i < 96; i++)
-    {
-        c1 = a1 + b1;
-        c2 = a2 + b2;
-        if (c2 > 999999999)
-        {
-            c1++;
-            c2 %= 1000000000;
-        }
-        printf(", ");
-        if (c1 == 0)
-        {
-            printf("%lu", c2);
-        }
-        else
-        {
-            printf("%lu%lu", c1, c2);
-        }
-        a1 = b1;
-        a2 = b2;
-        b1 = c1;
-        b2 = c2;
-        if (i == 95)
-        {
-            printf("\n%lu\n", c2);
-        }
-    }
 
-    return (0);
-}
+		printf("%lu", bef);
+
+
+		for (i = 1; i < 91; i++)
+		{
+			printf(", %lu", aft);
+			aft += bef;
+			bef = aft - bef;
+		}
+
+
+		bef1 = (bef / l);
+		bef2 = (bef % l);
+		aft1 = (aft / l);
+		aft2 = (aft % l);
+
+	for (i = 92; i < 99; ++i)
+		{
+			printf(", %lu", aft1 + (aft2 / l));
+			printf("%lu", aft2 % l);
+			aft1 = aft1 + bef1;
+			bef1 = aft1 - bef1;
+			aft2 = aft2 + bef2;
+			bef2 = aft2 - bef2;
+		}
+		printf("\n");
+		return (0);
+	}
